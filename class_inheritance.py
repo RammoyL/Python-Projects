e07ba53c360bc85d09096a01e7217a3da52d7f54
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-10-20 14:08:32 -0400

    Assignment
    Class inheritance sample

---
+++ class_inheritance.py
@@ -0,0 +1,16 @@
+
+class Vehicle:
+    Number of Doors = ''
+    Make = ''
+    Model = ''
+    Color = ''
+
+class Plane(Vehicle):
+    Number of wings = ''
+    propellers/jet engine = ''
+    Size Carrier = ''
+
+class Bike(Vehicle):
+    Number of wheels = ''
+    Frame = ''
+    
