4506a9b39663a4c3c44b07fcee0931350bc37fe8
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-11-05 04:55:28 -0400

    Create abstraction.py

---
+++ abstraction.py
@@ -0,0 +1,19 @@
+
+
+from abc import ABC, abstraction
+class home(ABC):
+    def rent(self, amount):
+        print("Total amount due: ",amount)
+#this function wants us to pass in an argument, but we won't indicate how or what kind of data
+    @abstraction
+    def payment(self, amount):
+        pass
+
+class debitPayment(home):
+#here is the payment function is defined and implimented from its parent class "rent".
+    def payment(self,amount):
+        print("Your recent payment of {} exceeds the limit of your current balance of $900 ".format(amount))
+
+obj = debitPayment()
+obj.rent("$985")
+obj.payment("$1,012")
