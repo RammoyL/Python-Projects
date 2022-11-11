a4f1d9b5b2a8eb135ab6c7f2650e5147e9c4bd0f
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-11-11 12:12:48 -0500

    Update encaps.py

--- encaps.py
+++ encaps.py
@@ -2,17 +2,19 @@ class Protected:
     def __init__(self):
         self._protectedVar = 21
 
+class Protected:
+    def __init__(self):
+        self.__privateVar = 21
+
     def getPrivate(self):
         print(self.__privateVar)
 
     def setPrivate(self, private):
         self.__privateVar = private
 
-thing = Protected()
-thing._protectedVar = 57
-print(thing._protectedVar)
-
 obj = Protected()
+obj._protectedVar = 0
+print(obj._protectedVar)
 obj.getPrivate()
 obj.setPrivate(39)
 obj.getPrivate
